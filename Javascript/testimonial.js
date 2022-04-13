b497b563765c4029b63e8e1693c5437e95e7687f
DMU-P2537469 <72402212+DMU-P2537469@users.noreply.github.com>
2022-04-13 08:32:03 +0100

    Update testimonial.js

--- Javascript/testimonial.js
+++ Javascript/testimonial.js
@@ -1,10 +1,15 @@
 
 // ============== Scrolling testimonies onclick moves the div left/right displaying covered info ==============
 
+
+// ============== 3 variables are defined: "testwrap", "indicators", "currentTestimonial". Within the html index page, the "testwrap" contains the testimonial contents: this being each specific testimonial card. The "indicators" contain the red buttons present under the testimonial written content and would be used to cycle between each testimony. The "currentTestimonial" would be a variable set to "0", this would act as a starting location ==============
+
 const testwrap = document.querySelector('.testwrap');
 const indicators = [...document.querySelectorAll('.indicators button')];
 
 let currentTestimonial = 0;
+
+// ============== When the "indicators" is clicked, the "indicators" would remove the current "testactive" class on the "indicators" and apply the "testactive" class to the specific "indicator" button clicked (In the css stylesheet the "testactive" is a slightly larger/stretched button to signal which testimony the user is currently viewing). The "testwraps" margin style would be adjusted to the left or right depending on which button is clicked and moved a calculated amount(`-${100 * i}%`) only to display the contents of the chosen testimony via the "indicator" buttons. the "currentTestimonial" is also given the identified as "i" within the equation ==============
 
 indicators.forEach((item, i) =>{
     item.addEventListener('click', () => {
