781543b336c98a84ce7cf097e5e69ea68b1c5d35
DMU-P2537469 <72402212+DMU-P2537469@users.noreply.github.com>
2022-04-13 08:34:18 +0100

    Update navscript.js

--- Javascript/navscript.js
+++ Javascript/navscript.js
@@ -1,6 +1,6 @@
 // ============== Drop down functionality ==============
 
-// ============== Each variable is defined, dropdown navLinks and links. each variable has a linked class with set animations and adjustments in the css stylesheet  ==============
+// ============== Each variable is defined: dropdown navLinks and links. each variable has a linked class with set animations and adjustments in the css stylesheet  ==============
 const dropdown = document.querySelector(".dropdown");
 const navLinks = document.querySelector(".nav-links");
 const links = document.querySelectorAll(".nav-links li");
@@ -17,7 +17,7 @@ dropdown.addEventListener('click', ()=>{
         link.classList.toggle("fade");
     });
 
-    // ============== The dropdown variable would be toggled with the "toggle" class inwhich the css stylesheet holds transform adjustments to start an animation adjusting the contents rotation and size etc. ==============
+    // ============== The dropdown variable would be toggled with the "toggle" class in which the css stylesheet holds transform adjustments to start an animation adjusting the rotation and size of the content etc. ==============
     
     //Animation
     dropdown.classList.toggle("toggle");
