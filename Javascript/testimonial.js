2658000aac6f98648da80d00ace9b76e409b0063
DMU-P2537469 <72402212+DMU-P2537469@users.noreply.github.com>
2022-04-13 08:33:18 +0100

    Update testimonial.js

--- Javascript/testimonial.js
+++ Javascript/testimonial.js
@@ -9,7 +9,7 @@ const indicators = [...document.querySelectorAll('.indicators button')];
 
 let currentTestimonial = 0;
 
-// ============== When the "indicators" is clicked, the "indicators" would remove the current "testactive" class on the "indicators" and apply the "testactive" class to the specific "indicator" button clicked (In the css stylesheet the "testactive" is a slightly larger/stretched button to signal which testimony the user is currently viewing). The "testwraps" margin style would be adjusted to the left or right depending on which button is clicked and moved a calculated amount(`-${100 * i}%`) only to display the contents of the chosen testimony via the "indicator" buttons. the "currentTestimonial" is also given the identified as "i" within the equation ==============
+// ============== When the "indicators" is clicked, the "indicators" would remove the current "testactive" class on the "indicators" and apply the "testactive" class to the specific "indicator" button clicked (In the css stylesheet the "testactive" is a slightly larger/stretched button to signal which testimony the user is currently viewing). The "testwraps" margin style would be adjusted to the left or right depending on which button is clicked and moved a calculated amount(`-${100 * i}%`) only to display the contents of the chosen testimony via the "indicator" buttons. the "currentTestimonial" is identified as "i" within the equation ==============
 
 indicators.forEach((item, i) =>{
     item.addEventListener('click', () => {
